3acc4d00d5e757c91a51bc5972c6dc139ce9747b
Keshav Prajapati <prajapatikeshav497@gmail.com>
2026-09-26 17:17:19 +0530

    Time: 1 ms (85.46%), Space: 43.1 MB (19.12%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -4,27 +4,19 @@ import java.util.Queue;
 class MyStack {
 
     Queue<Integer> q1;
-    Queue<Integer> q2;
 
     public MyStack() {
         q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
     }
 
     public void push(int x) {
+        int size = q1.size();
+        q1.offer(x);
 
-        // Put the new element into q2
-        q2.offer(x);
-
-        // Move all elements from q1 to q2
-        while (!q1.isEmpty()) {
-            q2.offer(q1.poll());
+        // rotate the queue so the newly added element ends up at the front
+        for (int i = 0; i < size; i++) {
+            q1.offer(q1.poll());
         }
-
-        // Swap q1 and q2
-        Queue<Integer> temp = q1;
-        q1 = q2;
-        q2 = temp;
     }
 
     public int pop() {
